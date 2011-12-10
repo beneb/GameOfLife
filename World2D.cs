95513f5ea5763a82d6625b0639cc894e1043a1e9
Benedikt Banas <banasb@yahoo.de>
2011-12-10 11:42:40 +0100

    Ignored some file _resharper for example and chaged name of the test file

---
+++ World2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameOfLife {
+    public class World2D {
+        private readonly int _xSize;
+        private readonly int _ySize;
+
+        private IEnumerable<ICell> _population;
+        private int[,] _world;
+
+        public World2D(int xSize, int ySize) {
+            _xSize = xSize;
+            _ySize = ySize;
+            _world = new int[_xSize,_ySize];
+        }
+
+        public void NextGneration() {
+            foreach (ICell cell in _population) {
+                cell.NextGeneration(GetNeighboursCount(cell));
+            }
+        }
+
+        private int GetNeighboursCount(ICell cell) {
+            throw new NotImplementedException();
+        }
+    }
+}
